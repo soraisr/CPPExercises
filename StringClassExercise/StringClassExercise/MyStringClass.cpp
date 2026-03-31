99b4fdc8515860cd0d75de9ab2d6df5d92d45c2e
Assaf Zohar <assafzohar@Assafs-MacBook-Air.local>
2026-03-31 18:08:43 +0300

    string cr

--- StringClassExercise/StringClassExercise/MyStringClass.cpp
+++ StringClassExercise/StringClassExercise/MyStringClass.cpp
@@ -1,6 +1,7 @@
 #include "MyStringClass.h"
 
 MyString::MyString() {
+	// CR: I don't know what I think about this as the default values
 	data = new char[1];
 	data[0] = '\0';
 	length = 0;
@@ -9,11 +10,13 @@ MyString::MyString() {
 
 MyString::MyString(const char* s) {
 	if(nullptr == s) {
+		// CR: do we want to support an asignment of a nullptr? I think this should throw an exception instead
 		data = new char[1];
 		data[0] = '\0';
 		length = 0;
 		occupiedSize = 1;
 	} else {
+		// CR: it's prettier to have the good code in the "happy flow"
 		length = getStrlen(s);
 		occupiedSize = length + 1;
 		data = new char[occupiedSize];
@@ -25,6 +28,7 @@ MyString::MyString(const char* s) {
 
 MyString::MyString(const MyString& other)
 {
+	// CR: there are some assumptions in this code - what if other.data is null?
 	length = other.length;
 	occupiedSize = other.occupiedSize;
 	data = new char[occupiedSize];
@@ -34,11 +38,13 @@ MyString::MyString(const MyString& other)
 }
 
 MyString::~MyString() {
+	// CR: you will read this in the future, but you should try catch your dtors, exceptions within exceptions cause for nasty flows
 	delete[] data;
 }
 
 MyString& MyString::operator=(const MyString& other)
 {
+	// CR: in the future you will see a pattern called copy and swap, which is better than what's going on here which is dangerous
 	if (this == &other) {
 		return *this;
 	}
@@ -50,6 +56,7 @@ MyString& MyString::operator=(const MyString& other)
 		newData[i] = other.data[i];
 	}
 
+	// CR: memory leak on fail
 	delete[] data;
 	data = newData;
 	length = other.length;
@@ -58,7 +65,7 @@ MyString& MyString::operator=(const MyString& other)
 	return *this;
 }
 	
-
+// CR: This method should be static
 size_t MyString::getStrlen(const char* s) const {
 	size_t i = 0;
 	while(s[i] != '\0') {
@@ -69,6 +76,7 @@ size_t MyString::getStrlen(const char* s) const {
 
 int MyString::find(const char* subStr) const {
 	if (nullptr == subStr) {
+		// CR: in cpp we throw when we see unsupported edge cases
 		return -1;
 	}
 
@@ -78,10 +86,12 @@ int MyString::find(const char* subStr) const {
 		return 0;
 	}
 
+	// CR: this should be above the first, and throw
 	if (subStrLen > length) {
 		return -1;
 	}
 
+	// CR: double nested loop is kinda ugly, maybe split into two functions
 	for (size_t i = 0; i <= length - subStrLen; i++) {
 		size_t j = 0;
 		while (j < subStrLen && data[i + j] == subStr[j]) {
@@ -99,11 +109,13 @@ void MyString::insert(const char* subStr, size_t index) {
 	if (nullptr == subStr) {
 		return;
 	}
+	// CR: good!
 	MyString subStrObj(subStr);
 	insert(subStrObj, index);
 }
 
 void MyString::insert(const MyString& other, size_t index){
+	// CR: good, here the assumption other can be null is checked
 	if (nullptr == other.data) {
 		return;
 	}
@@ -120,9 +132,11 @@ void MyString::insert(const MyString& other, size_t index){
 		newOccupiedSize = newLength + 1;
 	}
 
+	// CR: char* newData
+	// CR: indeed no need to check newData!=nullptr, do you know why?
 	char * newData = new char[newOccupiedSize];
 
-
+	// CR: this strcpy like code is here a lot, maybe it should be a function
 	for (size_t i = 0; i < index; i++) {
 		newData[i] = data[i];
 	}
@@ -137,6 +151,8 @@ void MyString::insert(const MyString& other, size_t index){
 
 	newData[newLength] = '\0';
 
+	// CR: there is some state danger here when stuff might fail
+	// if delete fails newdata is leaked, plus the dtor will try to delete this another time
 	delete[] data;
 	data = newData;
 	length = newLength;
@@ -144,6 +160,7 @@ void MyString::insert(const MyString& other, size_t index){
 }
 
 void MyString::remove(size_t index, size_t count) {
+	// CR: maybe this should throw
 	if (index >= length) {
 		return;
 	}
@@ -176,6 +193,7 @@ void MyString::append(const char* subStr) {
 
 void MyString::append(const MyString& other)
 {
+	// CR: why doesn't append just call insert in the last index?
 	if (nullptr == other.data) {
 		return;
 	}
